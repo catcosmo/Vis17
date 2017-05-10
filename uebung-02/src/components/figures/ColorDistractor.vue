25613a4388b3852ef3c5970cf35c4cd5cc5730b8
Arne Schlüter <arne@schlueter.is>
2017-05-10 16:40:48 +0200

    Add remaining distractors

---
+++ uebung-02/src/components/figures/ColorDistractor.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+  <svg width="320" height="320" viewBox="0 0 320 320">
+    <rect :x="coord[0]" :y="coord[1]" width="20" height="20" v-for="coord in coords.slice(0, coords.length - 1)" />
+    <rect :x="coords[coords.length - 1][0]" :y="coords[coords.length - 1][1]" :width="20" :height="20" class="special-color" />
+  </svg>
+</template>
+
+<script type="text/javascript">
+import randomCoords from '@/random-coords'
+
+export default {
+  name: 'color-distractor',
+
+  data () {
+    // generate 9 random coords
+    const coords = randomCoords({ amount: 9, maxX: 300, maxY: 300 })
+    return {
+      coords
+    }
+  }
+}
+</script>
---
+++ uebung-02/src/components/figures/SizeDistractor.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+  <svg width="320" height="320" viewBox="0 0 320 320">
+    <rect :x="coord[0]" :y="coord[1]" width="20" height="20" v-for="coord in coords.slice(0, coords.length - 1)" />
+    <rect :x="coords[coords.length - 1][0] - 20 / (scale / 2)" :y="coords[coords.length - 1][1] - 20 / (scale / 2)" :width="20 * scale" :height="20 * scale" fill="#000" />
+  </svg>
+</template>
+
+<script type="text/javascript">
+import randomCoords from '@/random-coords'
+
+export default {
+  name: 'size-distractor',
+
+  data () {
+    const coords = randomCoords({ amount: 9, maxX: 300, maxY: 300 })
+    const scale = 1.5 + Math.random()
+    return { coords, scale }
+  }
+}
+</script>
